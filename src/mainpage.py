fd17afc926de0a76ebe29f96d59b144e1751553c
QuietJohn0 <88064809+QuietJohn0@users.noreply.github.com>
2022-02-22 23:08:59 -0800

    new code for website

---
+++ src/mainpage.py
@@ -0,0 +1,8 @@
+## @file mainpage.py
+#  @author Rodolfo Diaz
+#  @author Daniel Munic
+#  @author John Bennett
+#  @date Febuary 7, 2022   
+#  @mainpage
+#
+#  @section Please Please Enjoy our Lab 3 Code!!
