dc5ea815764b596266a8c6801080cc38d008f470
QuietJohn0 <88064809+QuietJohn0@users.noreply.github.com>
2022-02-22 23:52:18 -0800

    website!!!

--- src/mainpage.py
+++ src/mainpage.py
@@ -2,7 +2,23 @@
 #  @author Rodolfo Diaz
 #  @author Daniel Munic
 #  @author John Bennett
-#  @date Febuary 7, 2022   
+#  @date Febuary 22, 2022   
 #  @mainpage
 #
-#  @section Please Please Enjoy our Lab 3 Code!!
+#  @section Software Software Design
+#  Here is our current plan for our code. First We have all our state and how they interact and share varables with each other.<br>
+#  \image html TaskDiagrams.png "Figure 1: Image Plotter Task Diagram" <br>
+#  Next sections show each States State Transition Diagram
+#  @subsection User User Task
+#  \image html FSMUser.png "Figure 2: UserTask State Transition Diagram" <br>
+#  @subsection Controller Controller Task
+#  \image html FSMController.png "Figure 3: Controller State Transition Diagram" <br>
+#  @subsection Encoder Encoder Task
+#  \image html FSMEncoder.png "Figure 4: Encoder State Transition Diagram" <br>
+#  @subsection Motor Motors Task
+#  \image html FSMMotors.png "Figure 5: Motor State Transition Diagram" <br>
+#  @subsection Logic Logic Task
+#  \image html FSMLogic.png "Figure 6: Logic State Transition Diagram" <br>
+#  @subsection Solenoid Solenoid Task
+#  \image html FSMSolenoid.png "Figure 7: Solenoid State Transition Diagram" <br>
+#  
